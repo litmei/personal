8ba2ae3f2265a1ff0a234609c2e4e7715a5a7674
Kurkur <mccllm@qq.com>
2026-08-24 09:38:56 +0800

    benchmark_sharegpt.sh

---
+++ scripts/sglang/bench/benchmark_sharegpt.sh
@@ -0,0 +1,13 @@
+python3 -m sglang.bench_serving \
+	--dataset-name random \
+	--apply-chat-template \
+	--random-input 2000 \
+	--random-output 500 \
+	--random-range-ratio 1.0 \
+	--model /home/weights/Kimi-K2.5-w4a8 \
+	--backend sglang \
+	--max-concurrency 24 \
+	--num-prompts 48 \
+	--host 127.0.0.1 \
+	--port 8880 \
+	--dataset-path ./ShareGPT_V3_unfiltered_cleaned_split.json
